85fab2b1652f14de990afea92ff7670b81b0582b
Matthew Watman <mattwatman@gmail.com>
2022-06-27 19:44:37 -0400

    Update common pool allocation logic

--- packages/backend/contracts/CommunityCare.sol
+++ packages/backend/contracts/CommunityCare.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.8.14;
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "./ERC20.sol";
 
 contract CommunityCare {
@@ -35,26 +36,30 @@ contract CommunityCare {
         Phases currentPhase;
         uint totalFundsRequested;
         uint totalDonations;
+        uint totalDonationsToRequests;
         uint totalFundsInWei;
         uint totalSettlementsInWei;
 
     }
 
     struct Request {
+        string requestId;
         address requester;
         uint requestTime;
         string title;
         string description;
         uint requestAmountInWei;
         string supportingDocumentation;
+        uint numberDonations;
         uint amountFundedInWei;
         bool hasBeenSettled;
     }
 
     struct Donation {
+        address donator;
         uint donationTime;
         uint donationAmountInWei;
-        bool donationToRequest
+        string requestId;
     }
     //Long integers to allow for ratios below 1
     struct RTDRatio {
@@ -78,7 +83,7 @@ contract CommunityCare {
     event PhaseStarted(uint indexed roundNumber, Phases indexed phase);
     event RequestCreated(address indexed requester, uint indexed requestAmountInWei);
     event DonationToCommonPoolCreated(address indexed donator, uint indexed donationAmountInWei);
-    event DonationToRequestCreated(address indexed donator, uint indexed donationAmountInWei, uint indexed requestId);
+    event DonationToRequestCreated(address indexed donator, uint indexed donationAmountInWei, string indexed requestTitle);
     event TokenRewardsGenerated(address indexed requester, uint indexed rewardAmountInWei);
     event TokenRewardsWithdrawn(address indexed requester, uint indexed rewardAmountInWei);
     event FundingAllocated(address[] indexed requesters, uint currentRoundNumber);
@@ -126,6 +131,7 @@ contract CommunityCare {
             0,
             0,
             0,
+            0,
             0
         );
         rounds.push(newRound);
@@ -137,12 +143,14 @@ contract CommunityCare {
 
         uint currentRoundNumber = rounds.length - 1;
         Request memory newRequest = Request(
+            string.concat(Strings.toHexString(uint160(msg.sender)), "-", Strings.toString(block.timestamp))
             msg.sender,
             block.timestamp,
             title,
             description,
             requestAmountInWei,
             supportingDocumentation,
+            0,
             0,
             false
         );
@@ -155,72 +163,25 @@ contract CommunityCare {
         emit RequestCreated(msg.sender, requestAmountInWei);
     }
 
-    function donateToRequest(uint _requestId) public payable checkTime onlyPhase(Phases.Funding) {
+    function donate(string memory _requestId) public payable checkTime onlyPhase(Phases.Funding) {
         require(msg.value > 0, "Donation amount must be greater than zero");
-
-        uint currentRoundNumber = rounds.length - 1;
-        uint donationTime = block.timestamp;
-
-        Donation memory newDonation = Donation(
-            donationTime,
-            msg.value,
-            true
-        );
-
-        donations[msg.sender][currentRoundNumber].push(newDonation);
-        rounds[currentRoundNumber].totalDonations++;
-        rounds[currentRoundNumber].totalFundsInWei += msg.value;
-        requestToDonationRatios[msg.sender].numberDonations += 1e18;
-        EnumerableSet.contains(donators, msg.sender) ? false : donators.add(msg.sender);
-
-        //Calculate rewards and add to reward balance
-        uint tokenRewards = _calculateTokenRewards(msg.sender, msg.value);
-        if (tokenRewards > 0) {
-            rewardBalances[msg.sender] += tokenRewards;
-            emit TokenRewardsGenerated(msg.sender, tokenRewards);
-        }
-
+        _donate(msg.sender, msg.value, _requestId);
         emit DonationToRequestCreated(msg.sender, msg.value, _requestId);
     }
 
-    function donateToCommonPool() public payable checkTime onlyPhase(Phases.Funding){
+    function donate() public payable checkTime onlyPhase(Phases.Funding){
         require(msg.value > 0, "Donation amount must be greater than zero");
-
-        uint currentRoundNumber = rounds.length - 1;
-        uint donationTime = block.timestamp;
-
-        Donation memory newDonation = Donation(
-            donationTime,
-            msg.value,
-            false
-        );
-
-        donations[msg.sender][currentRoundNumber].push(newDonation);
-        rounds[currentRoundNumber].totalDonations++;
-        rounds[currentRoundNumber].totalFundsInWei += msg.value;
-        commonPoolBalanceInWei += msg.value;
-        requestToDonationRatios[msg.sender].numberDonations += 1e18;
-        EnumerableSet.contains(donators, msg.sender) ? false : donators.add(msg.sender);
-
-        //Calculate rewards and add to reward balance
-        uint tokenRewards = _calculateTokenRewards(msg.sender, msg.value);
-        if (tokenRewards > 0) {
-            rewardBalances[msg.sender] += tokenRewards;
-            emit TokenRewardsGenerated(msg.sender, tokenRewards);
-        }
-
+        _donate(msg.sender, msg.value, "");
         emit DonationToCommonPoolCreated(msg.sender, msg.value);
     }
 
     function allocateFundingPool() public checkTime onlyPhase(Phases.Allocation) {
         require(rounds[rounds.length - 1].totalFundsInWei > 0, "No funding to allocate");
-
         address[] memory requestorsArray = EnumerableSet.values(requestors);
         uint currentRoundNumber = rounds.length - 1;
         _allocateFunding(requestorsArray, currentRoundNumber);
-
         emit FundingAllocated(requestorsArray, currentRoundNumber);
-        }
+    }
 
     function settleRequests() public checkTime onlyPhase(Phases.Settlement){
         require(requests[msg.sender][rounds.length - 1].length > 0, "No requests to settle");
@@ -305,17 +266,57 @@ contract CommunityCare {
     //Simple algorithm for funding for now but can be replaced in the future
     function _allocateFunding(address[] memory _requestorsArray, uint _currentRoundNumber) internal {
         for (uint i = 0; i < _requestorsArray.length; i++) {
-            Request[] storage requestsArray = requests[_requestorsArray[i]][rounds.length - 1];
-            for (uint j = 0; j < requestsArray.length; j++ ){
-                if (rounds[_currentRoundNumber].totalFundsInWei >= rounds[_currentRoundNumber].totalFundsRequested) {
-                    requestsArray[j].amountFundedInWei = requestsArray[j].requestAmountInWei;
-                } else {
-                    requestsArray[j].amountFundedInWei = rounds[_currentRoundNumber].totalFundsInWei / rounds[_currentRoundNumber].totalRequests;
-                }
+            address requester = _requestorsArray[i];
+            for(uint j = 0; j < requests[requester][_currentRoundNumber].length; j++) {
+                Request storage request = requests[requester][_currentRoundNumber][j];
+                uint256 commonPoolBalanceCopy = commonPoolBalanceInWei;
+                uint256 requestNumberOfDonations = request.numberOfDonations;
+                uint256 totalDonationsInRound = 0;
+                uint256 allocation = commonPoolBalanceCopy/ (requestNumberOfDonations / totalDonationsInRound);
+                request.amountFundedInWei += allocation;
             }
         }
+
     }
 
+    function _donate(address _donator, uint _donationAmount, string memory _requestId) internal {
+        
+        uint currentRoundNumber = rounds.length - 1;
+        uint donationTime = block.timestamp;
+
+        Donation memory newDonation = Donation(
+            msg.sender
+            donationTime,
+            msg.value,
+            requestId
+        );
+
+        donations[msg.sender][currentRoundNumber].push(newDonation);
+        if (_requestId.length > 0) {
+            Request memory requestArray = requests[msg.sender][currentRoundNumber];
+            for (uint i = 0; i < requestArray.length; i++) {
+                if (requestArray[i].requestId == _requestId) {
+                    requestArray[i].amountFundedInWei += msg.value;
+                    requestArray[i].numberDonations++;
+                }
+            }
+        } else {
+            commonPoolBalanceInWei += msg.value;
+        }
+        requestToDonationRatios[msg.sender].numberDonations += 1e18;
+        EnumerableSet.contains(donators, msg.sender) ? false : donators.add(msg.sender);
+
+        rounds[currentRoundNumber].totalDonations++;
+        rounds[currentRoundNumber].totalFundsInWei += msg.value;
+
+        //Calculate rewards and add to reward balance
+        uint tokenRewards = _calculateTokenRewards(msg.sender, msg.value);
+        if (tokenRewards > 0) {
+            rewardBalances[msg.sender] += tokenRewards;
+            emit TokenRewardsGenerated(msg.sender, tokenRewards);
+        }
+
+    }
 
     //Simple algorithm for token rewards for now but can be replaced in the future
     function _calculateTokenRewards(address donator, uint donationAmount) internal view returns (uint tokenRewards) {
@@ -330,5 +331,9 @@ contract CommunityCare {
         Round memory currentRound = rounds[rounds.length - 1];
         currentRound.currentPhase = Phases(uint(currentRound.currentPhase) + 1);
         emit PhaseStarted(rounds.length - 1, currentRound.currentPhase);
+    }
+
+    function _cleanUp() internal {
+
     }
 }
